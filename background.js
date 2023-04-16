e0ae96eb26c11b411bc8e8dc567112841d345723
devlulcas <lucasalvesrego.contato@gmail.com>
2023-04-16 02:31:22 -0300

    feat: save toggle state

--- background.js
+++ background.js
@@ -1,34 +1,51 @@
-const url = "https://www.youtube.com/";
-
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ state: 'HIDDEN' });
+
   chrome.action.setBadgeText({
-    text: "VISIBLE",
+    text: 'HIDDEN',
   });
 });
 
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(url)) {
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-
-    const nextState = prevState === "HIDDEN" ? "VISIBLE" : "HIDDEN";
-
-    await chrome.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    await chrome.storage.sync.get('state', (result) => {
+      if (result.state === 'HIDDEN') {
+        toggleInjection(tabId, 'HIDDEN');
+      } else {
+        console.log('VISIBLE');
+        toggleInjection(tabId, 'VISIBLE');
+      }
     });
-
-    const css = "injectable.css";
-
-    if (nextState === "HIDDEN") {
-      await chrome.scripting.insertCSS({
-        files: [css],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === "VISIBLE") {
-      await chrome.scripting.removeCSS({
-        files: [css],
-        target: { tabId: tab.id },
-      });
-    }
   }
 });
+
+chrome.action.onClicked.addListener(async (tab) => {
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+
+  const nextState = prevState === 'HIDDEN' ? 'VISIBLE' : 'HIDDEN';
+
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+
+  chrome.storage.sync.set({ state: nextState });
+
+  toggleInjection(tab.id, nextState);
+});
+
+async function toggleInjection(tabId, state) {
+  const css = 'injectable.css';
+
+  if (state === 'HIDDEN') {
+    await chrome.scripting.insertCSS({
+      files: [css],
+      target: { tabId },
+    });
+  } else {
+    await chrome.scripting.removeCSS({
+      files: [css],
+      target: { tabId },
+    });
+  }
+}
